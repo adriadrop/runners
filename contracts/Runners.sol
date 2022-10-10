cfcda1e7ecb9ce699991c5136d9c0af0883542c8
marko.b <marko.b@lelo.com>
2022-10-10 18:48:57 +0200

    remove not needed code

--- contracts/Runners.sol
+++ contracts/Runners.sol
@@ -3,19 +3,13 @@
 pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
-import "base64-sol/base64.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-error ERC721Metadata__URI_QueryFor_NonExistentToken();
-error NFT_NOT_A_OWNER();
-
-contract Runners is ERC721A, Ownable {
+contract Runners is ERC721A {
     // Sudo NFT variables
     bool minted;
     string constant ipfs = "ipfs://QmUeA3dbnDrZ8hVLYVF3BEwWR2tyibxJr1dQgV5Acd1eCe/";
 
-    constructor() ERC721A("Blade City Runners", "BCR") {}
+    constructor() ERC721A("Blade City Runners", "BRUU") {}
 
     function _baseURI() internal pure override returns (string memory) {
         return ipfs;
